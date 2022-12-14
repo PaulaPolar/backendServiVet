cf44278e677c13481d676358eea3dfdda6d74ed3
alejodevelop <65092704+alejocabarcas@users.noreply.github.com>
2022-12-14 17:48:49 -0500

    FEATURES: added missing queries  to the db

--- db/dbCalls.js
+++ db/dbCalls.js
@@ -47,6 +47,271 @@ const getUsuarios = async () => {
     return data;
 }
 
+const getVeterinaria = async (id) => {
+
+    data = await db.one(`SELECT * FROM public.veterinaria WHERE id_veterinaria = ${id}`)
+        .then((data) => {
+            //console.log('DATA:', data)
+            return data
+        })
+        .catch((error) => {
+            console.log('ERROR:', error)
+        })
+    return data;
+}
+
+const getVeterinarias = async () => {
+
+    data = await db.query(`SELECT * FROM public.veterinaria`)
+        .then((data) => {
+            //console.log('DATA:', data)
+            return data
+        })
+        .catch((error) => {
+            console.log('ERROR:', error)
+        })
+    return data;
+}
+
+// create all the calls to the database in here
+// remember to finish the fronent shit that fucking paula is mogglin about
+
+const getEstadoCita = async (id) => {
+
+    data = await db.one(`SELECT * FROM public.estado_cita WHERE id_estado_cita = ${id}`)
+        .then((data) => {
+            //console.log('DATA:', data)
+            return data
+        })
+        .catch((error) => {
+            console.log('ERROR:', error)
+        })
+    return data;
+}
+
+const getEstadoCitas = async () => {
+
+    data = await db.query(`SELECT * FROM public.estado_cita`)
+        .then((data) => {
+            //console.log('DATA:', data)
+            return data
+        })
+        .catch((error) => {
+            console.log('ERROR:', error)
+        })
+    return data;
+}
+
+const getCita = async (id) => {
+
+    data = await db.one(`SELECT * FROM public.cita WHERE id_cita = ${id}`)
+        .then((data) => {
+            //console.log('DATA:', data)
+            return data
+        })
+        .catch((error) => {
+            console.log('ERROR:', error)
+        })
+    return data;
+}
+
+const getCitas = async () => {
+
+    data = await db.query(`SELECT * FROM public.cita`)
+        .then((data) => {
+            //console.log('DATA:', data)
+            return data
+        })
+        .catch((error) => {
+            console.log('ERROR:', error)
+        })
+    return data;
+}
+
+const getPais = async (id) => {
+
+    data = await db.one(`SELECT * FROM public.pais WHERE id_pais = ${id}`)
+        .then((data) => {
+            //console.log('DATA:', data)
+            return data
+        })
+        .catch((error) => {
+            console.log('ERROR:', error)
+        })
+    return data;
+}
+
+const getPaises = async () => {
+
+    data = await db.query(`SELECT * FROM public.pais`)
+        .then((data) => {
+            //console.log('DATA:', data)
+            return data
+        })
+        .catch((error) => {
+            console.log('ERROR:', error)
+        })
+    return data;
+}
+
+const getRol = async (id) => {
+
+    data = await db.one(`SELECT * FROM public.rol WHERE id_rol = ${id}`)
+        .then((data) => {
+            //console.log('DATA:', data)
+            return data
+        })
+        .catch((error) => {
+            console.log('ERROR:', error)
+        })
+    return data;
+}
+
+const getRoles = async () => {
+
+    data = await db.query(`SELECT * FROM public.rol`)
+        .then((data) => {
+            //console.log('DATA:', data)
+            return data
+        })
+        .catch((error) => {
+            console.log('ERROR:', error)
+        })
+    return data;
+}
+
+const getEspacioCalendario = async (id) => {
+
+    data = await db.one(`SELECT * FROM public.espacio_calendario WHERE id_espacio_calendario = ${id}`)
+        .then((data) => {
+            //console.log('DATA:', data)
+            return data
+        })
+        .catch((error) => {
+            console.log('ERROR:', error)
+        })
+    return data;
+}
+
+const getEspacioCalendarios = async () => {
+
+    data = await db.query(`SELECT * FROM public.espacio_calendario`)
+        .then((data) => {
+            //console.log('DATA:', data)
+            return data
+        })
+        .catch((error) => {
+            console.log('ERROR:', error)
+        })
+    return data;
+}
+
+// const getImagenCarruselVeterinaria = async (id) => {
+
+//     data = await db.one(`SELECT * FROM public.espacio_calendario WHERE id_espacio_calendario = ${id}`)
+//         .then((data) => {
+//             //console.log('DATA:', data)
+//             return data
+//         })
+//         .catch((error) => {
+//             console.log('ERROR:', error)
+//         })
+//     return data;
+// }
+
+// const getImagenesCarruselVeterinarias = async () => {
+
+//     data = await db.query(`SELECT * FROM public.espacio_calendario`)
+//         .then((data) => {
+//             //console.log('DATA:', data)
+//             return data
+//         })
+//         .catch((error) => {
+//             console.log('ERROR:', error)
+//         })
+//     return data;
+// }
+
+const getServicioVeterinaria = async (id) => {
+
+    data = await db.one(`SELECT * FROM public.servicio_veterinaria WHERE id_servicio_veterinaria = ${id}`)
+        .then((data) => {
+            //console.log('DATA:', data)
+            return data
+        })
+        .catch((error) => {
+            console.log('ERROR:', error)
+        })
+    return data;
+}
+
+const getServiciosVeterinarias = async () => {
+
+    data = await db.query(`SELECT * FROM public.servicio_veterinaria`)
+        .then((data) => {
+            //console.log('DATA:', data)
+            return data
+        })
+        .catch((error) => {
+            console.log('ERROR:', error)
+        })
+    return data;
+}
+
+const getProducto = async (id) => {
+
+    data = await db.one(`SELECT * FROM public.producto WHERE id_producto = ${id}`)
+        .then((data) => {
+            //console.log('DATA:', data)
+            return data
+        })
+        .catch((error) => {
+            console.log('ERROR:', error)
+        })
+    return data;
+}
+
+const getProductos = async () => {
+
+    data = await db.query(`SELECT * FROM public.producto`)
+        .then((data) => {
+            //console.log('DATA:', data)
+            return data
+        })
+        .catch((error) => {
+            console.log('ERROR:', error)
+        })
+    return data;
+}
+
+const getImagen = async (id) => {
+
+    data = await db.one(`SELECT * FROM public.imagen WHERE id_imagen = ${id}`)
+        .then((data) => {
+            //console.log('DATA:', data)
+            return data
+        })
+        .catch((error) => {
+            console.log('ERROR:', error)
+        })
+    return data;
+}
+
+const getImagenes = async () => {
+
+    data = await db.query(`SELECT * FROM public.imagen`)
+        .then((data) => {
+            //console.log('DATA:', data)
+            return data
+        })
+        .catch((error) => {
+            console.log('ERROR:', error)
+        })
+    return data;
+}
+
+
+
 
 const createUsuario = async (
     correo,
@@ -107,6 +372,24 @@ const createUsuario = async (
 
 
 module.exports = {
+    getCita,
+    getCitas,
+    getEspacioCalendario,
+    getEspacioCalendarios,
+    getEstadoCita,
+    getEstadoCitas,
+    getImagen,
+    getImagenes,
+    getPais,
+    getPaises,
+    getProducto,
+    getProductos,
+    getRol,
+    getRoles,
+    getServicioVeterinaria,
+    getServiciosVeterinarias,
+    getVeterinaria,
+    getVeterinarias,
     getUsuario,
     getUsuarios,
     createUsuario,
